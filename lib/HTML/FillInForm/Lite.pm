6912af901842cb0663ab8d299d6e9fb4f8d09d59
Fuji, Goro <fuji.goro@dena.jp>
2012-06-25 20:02:35 +0900

    Checking in changes prior to tagging of version 1.10.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 62175b5..cff54ff 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension HTML::FillInForm::Lite.
    
    +1.10 2012-06-25 20:01:26
    +    - allow data-* attributes (github issue #1)
    +
     1.09 Mon Jun 28 18:47:44 2010
     	- Add an exportable function fillinform()
     	- Make fill() accept string-like objects as sources

--- lib/HTML/FillInForm/Lite.pm
+++ lib/HTML/FillInForm/Lite.pm
@@ -4,7 +4,7 @@ use 5.006_000; # 5.6.0
 use strict;
 use warnings;
 
-our $VERSION  = '1.09';
+our $VERSION  = '1.10';
 
 use Exporter ();
 our @ISA       = qw(Exporter);
@@ -649,7 +649,7 @@ HTML::FillInForm::Lite - Lightweight FillInForm module in Pure Perl
 
 =head1 VERSION
 
-The document describes HTML::FillInForm::Lite version 1.09
+The document describes HTML::FillInForm::Lite version 1.10
 
 =head1 SYNOPSIS
 
