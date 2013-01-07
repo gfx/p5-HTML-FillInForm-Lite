9ccc310cb01d9c963a4dc770e79158f6c113d750
Fuji, Goro <g.psy.va@gmail.com>
2013-01-07 14:31:16 -0800

    Checking in changes prior to tagging of version 1.12.
    Changelog diff is:

--- lib/HTML/FillInForm/Lite/Compat.pm
+++ lib/HTML/FillInForm/Lite/Compat.pm
@@ -3,7 +3,7 @@ package HTML::FillInForm::Lite::Compat;
 use strict;
 use warnings;
 
-our $VERSION = '1.11';
+our $VERSION = '1.12';
 
 use HTML::FillInForm::Lite;
 our @ISA = qw(HTML::FillInForm::Lite);
